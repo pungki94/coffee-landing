6db7062c66464f420ced3a8f696b88fceaebf227
pungki wibowo <pungkiwibowo29@gmail.com>
2025-11-30 09:58:42 +0700

    update url dan home

--- src/App.tsx
+++ src/App.tsx
@@ -7,35 +7,28 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Shop from "./pages/Shop";
 import Contact from "./pages/Contact";
+import { Product } from "./types/product";
 
-export interface Product {
-  id: number;
-  name: string;
-  price: number;
-  image: string;
-  description: string;
-  category: string;
-  cartId?: string; // untuk identitas unik item
+// MUST match Navbar: Product + qty
+export interface CartItem extends Product {
+  qty: number;
 }
 
 function App() {
-  // ❗ INI SATU-SATUNYA STATE CART
-  const [cart, setCart] = useState<Product[]>([]);
+  // gunakan CartItem[], bukan Product[]
+  const [cart, setCart] = useState<CartItem[]>([]);
 
   return (
-    <Router>
-      <Navbar cart={cart} setCart={setCart} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route
-          path="/shop"
-          element={<Shop cart={cart} setCart={setCart} />}
-        />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </Router>
+   <Router basename="/coffee-landing">
+   <Navbar cart={cart} setCart={setCart} />
+   <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/shop" element={<Shop cart={cart} setCart={setCart} />} />
+      <Route path="/contact" element={<Contact />} />
+   </Routes>
+   <Footer />
+</Router>
   );
 }
 
